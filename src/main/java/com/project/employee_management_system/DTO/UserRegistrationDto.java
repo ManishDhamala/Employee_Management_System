e2d8bc815b8e7dcaf3b8e9ec6b4324751bf6c679
ManishDhamala <dhamalamanish32@gmail.com>
2025-03-04 15:53:22 +0545

    Security Configuration

--- src/main/java/com/project/employee_management_system/DTO/UserRegistrationDto.java
+++ src/main/java/com/project/employee_management_system/DTO/UserRegistrationDto.java
@@ -1,10 +1,14 @@
 package com.project.employee_management_system.DTO;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegistrationDto {
     private String firstName;
     private String lastName;
@@ -12,35 +16,35 @@ public class UserRegistrationDto {
     private String password;
 
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 }
