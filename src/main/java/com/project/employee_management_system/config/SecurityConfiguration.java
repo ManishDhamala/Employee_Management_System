98f87a8c2381b8025ec42558a739e2d931ee650f
ManishDhamala <dhamalamanish32@gmail.com>
2025-03-05 13:12:23 +0545

    Configure global authentication manager

--- src/main/java/com/project/employee_management_system/config/SecurityConfiguration.java
+++ src/main/java/com/project/employee_management_system/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.project.employee_management_system.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,6 +40,11 @@ public class SecurityConfiguration {
                     );
             return http.build();
         }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
 
 
 }
