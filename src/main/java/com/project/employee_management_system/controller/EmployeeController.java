7934766e056bd46f0e2c053b41261a36ea981179
ManishDhamala <dhamalamanish32@gmail.com>
2025-03-06 15:16:53 +0545

    Employee REST Controller

---
+++ src/main/java/com/project/employee_management_system/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.project.employee_management_system.controller;
+
+import com.project.employee_management_system.model.Employee;
+import com.project.employee_management_system.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping()
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees,HttpStatus.OK );
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> saveEmployee(Employee employee) {
+        employeeService.saveEmployee(employee);
+        return new ResponseEntity<>(employee,HttpStatus.CREATED);
+    }
+
+}
