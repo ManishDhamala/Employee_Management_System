52a2603270abe07e65894c9e82be8114b739235f
ManishDhamala <dhamalamanish32@gmail.com>
2025-03-07 15:57:29 +0545

    Updated Security Configuration

--- src/main/java/com/project/employee_management_system/config/SecurityConfiguration.java
+++ src/main/java/com/project/employee_management_system/config/SecurityConfiguration.java
@@ -1,26 +1,39 @@
 package com.project.employee_management_system.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
-        @Autowired
-        private UserDetailsService userDetailsService;
+
+        private final UserDetailsService userDetailsService;
 
         @Bean
         public BCryptPasswordEncoder passwordEncoder() {
             return new BCryptPasswordEncoder();
         }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(List.of(authProvider));
+    }
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -40,11 +53,6 @@ public class SecurityConfiguration {
                     );
             return http.build();
         }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
 
 
 }
--- src/main/java/com/project/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/project/employee_management_system/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.project.employee_management_system.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,6 @@ public class EmployeeController {
         employeeService.saveEmployee(employee);
         return new ResponseEntity<>(employee,HttpStatus.CREATED);
     }
+
 
 }
